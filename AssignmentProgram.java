96cd797e36a82549e15a46557547d74272457567
kanthimathi2u <43551069+kanthimathi2u@users.noreply.github.com>
2018-09-25 03:15:14 +0530

    Add files via upload

---
+++ AssignmentProgram.java
@@ -0,0 +1,22 @@
+package com.org.app.assignment;
+
+public class AssignmentProgram{
+
+	/**
+	 * If no argument is provided then the input file present inside main.com.app.logic package is
+	 * picked up as input file by default.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String filePath = null;
+		if (args.length != 0)
+			filePath = args[0];
+		try{
+			
+		}
+		catch(Exception e){
+			System.out.println("Something went wrong....File Not Found ");
+		}
+	}
+
+}
